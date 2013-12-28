042db5df9bb563cdac0bc924ab3e7d2247e6b354
Louis Wilbrink Air <louis.wilbrink@gmail.com>
2013-12-28 01:09:44 -0500

    created d3 pie menu from example, using SVG created using js

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('radialMenuApp', [
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
