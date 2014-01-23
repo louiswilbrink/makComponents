0ede49e136e00c1b6534d4851cc358c04275febd
Louis Wilbrink Air <louis.wilbrink@gmail.com>
2014-01-23 16:57:40 -0500

    Finished Scales

--- app/scripts/directives/scales.js
+++ app/scripts/directives/scales.js
@@ -36,13 +36,101 @@ angular.module('radialMenuApp')
 
             $scope.scales.scaledOutputs.push([input, scaledOutput]);
           });
+        };
 
-          console.log($scope.scales.scaledOutputs);
+        var drawScatterPlot = function () {
+          
+          $scope.scales.width = 500;
+          var height = 300,
+              padding = 20;
+
+          var dataset = [
+            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88],
+            [600, 150]
+          ];
+
+          // Create a linear scale for the x axis.
+          var xScale = d3.scale.linear()
+            .domain([ 0, d3.max(dataset, function (d) {
+              return d[0];
+            })])
+            // Incorporate padding (extra right-padding for labels).
+            .range([ padding , $scope.scales.width - (padding * 3)])
+
+          // Create an *inverted* linear scale for the y axis.
+          var yScale = d3.scale.linear()
+            .domain([ 0, d3.max(dataset, function (d) {
+              return d[1];
+            })])
+            // Inversion (higher y values appear higher on the scatter plot).
+            // Incorporate padding.
+            .range([ height - padding, padding ])
+
+          // Create a linear scale for circle radius (min 2, max 5).
+          var rScale = d3.scale.linear()
+            .domain([ 0, d3.max(dataset, function (d) {
+              return d[1];
+            })])
+            .range([ 2, 5 ])
+
+          var color = d3.scale.category10();
+
+          // Create SVG element.
+          var svg = d3.select('.scales')
+            .select('.sandbox')
+            .append('svg')
+              .attr('width', $scope.scales.width)
+              .attr('height', height)
+              .style("border", "1px solid black")
+
+          // Draw circles according to dataset.
+          var circles = svg.selectAll('circle')
+            .data(dataset)
+            .enter()
+            .append('circle')
+              // Position.
+              .attr('cx', function (d) {
+                return xScale(d[0]);
+              })
+              .attr('cy', function (d) {
+                return yScale(d[1]);
+              })
+              // Set scaled radius and color.
+              .attr('r', function (d) {
+                return rScale(d[1]);
+              })
+              .attr('fill', function (d, i) {
+                return color(i);
+              })
+
+          // Draw corresponding labels.
+          var labels = svg.selectAll('text')
+            .data(dataset)
+            .enter()
+            .append('text')
+              // Position.
+              .attr('x', function (d) {
+                return xScale(d[0]);
+              })
+              .attr('y', function (d) {
+                return yScale(d[1]);
+              })
+              // Text, style.
+              .text(function (d) {
+                return d[0] + ', ' + d[1];
+              })
+                .attr({
+                  'font-family': 'Fondamento',
+                  'font-size': '15px',
+                  'fill': 'red'
+                })
         };
 
         // Initialization.
        
         simpleScale();
+        drawScatterPlot();
         
       }
     };
