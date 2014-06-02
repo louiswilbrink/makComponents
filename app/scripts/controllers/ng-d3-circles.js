be8e875eabde34ae1e93617adf51135f14b32979
Louis Wilbrink Air <louis.wilbrink@gmail.com>
2014-06-01 23:41:50 -0400

    calculating position of revolving circle

--- app/scripts/controllers/ng-d3-circles.js
+++ app/scripts/controllers/ng-d3-circles.js
@@ -3,6 +3,8 @@
 angular.module('makComponents')
   .controller('NgD3CirclesCtrl', function ($scope, $timeout) {
 
+    var theta = 0;
+
     $scope.circles = [
       {
         cx: 100,
@@ -30,12 +32,26 @@ angular.module('makComponents')
       }
     ];
 
-    $timeout(function () {
-      $scope.circles[0] = {
-        cx: 500,
-        cy: 500,
-        r: 20,
-        fill: 'blue'
-      };
-    }, 2000);
+    function incrementTheta () {
+      theta += Math.PI / 180;
+
+      if (theta === (Math.PI / 2)) {
+        console.log('PI / 2');
+      }
+      else if (theta === (Math.PI)) {
+        console.log('PI');
+      }
+      else if (theta === ((3 * Math.PI) / 2)) {
+        console.log('3PI / 2');
+      }
+      else if (theta === (2 * Math.PI)) {
+        console.log('2PI');
+        theta = 0;
+      }
+
+      // Exceeds maximum call stack.
+      //$timeout(incrementTheta(), 1000);
+    };
+
+    incrementTheta();
   });
